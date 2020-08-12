531a7a637ac70d920115f1e61ad4b06f6e94ffd0
2600 <65238210+2600projects@users.noreply.github.com>
2020-08-12 14:15:49 -0400

    Bring RGB-to_opt up to 3.1 zip

--- RGB-to_opt/retropie/configs/all/runcommand-onend.sh
+++ RGB-to_opt/retropie/configs/all/runcommand-onend.sh
@@ -1,5 +1,5 @@
-# when exiting an emulator -- revert to 320x240p
-#vcgencmd hdmi_timings 320 1 23 30 34 240 1 4 3 15 1 0 0 60 0 6400000 1 > /dev/null #Retrotink 320x240p Timing Original
-vcgencmd hdmi_timings 320 1 15 30 42 240 1 4 3 15 1 0 0 60 0 6400000 1 > /dev/null #RetroTINK 320x240p Timing Adjusted
-tvservice -e "DMT 87" > /dev/null
-fbset -depth 8 && fbset -depth 24 -xres 320 -yres 240 > /dev/null #Retrotink 24b Depth
+## when exiting an emulator -- revert to 320x240p
+	vcgencmd hdmi_timings 320 1 15 30 42 240 1 4 3 15 1 0 0 60 0 6400000 1 > /dev/null #CRTPi 320x240p Timing Adjusted
+	tvservice -c "DMT 87" 
+	fbset -depth 8 && fbset -depth 16 && fbset -depth 24 -xres 320 -yres 240 > /dev/null #24b depth
+	
