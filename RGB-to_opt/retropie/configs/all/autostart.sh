ea40ba3773368c7ff57218d51715bfcdca640485
2600 <65238210+2600projects@users.noreply.github.com>
2020-08-16 10:25:12 -0400

    Add autostart.sh file

---
+++ RGB-to_opt/retropie/configs/all/autostart.sh
@@ -0,0 +1,10 @@
+#Uncomment below to launch EmulationStation
+emulationstation --no-splash #auto
+
+#Uncomment below to launch Attract Mode
+#setterm -cursor off
+#stty -echo
+#clear
+#attract 2>&1 | grep -o $'\33\[2j'
+#stty sane
+
